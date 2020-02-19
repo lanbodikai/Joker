69d6502610336e9da9fcb8a1560afa491702d8e5
jammind <jammind@gmail.com>
2020-02-19 12:32:37 +0800

    Arduino codes fixed

--- ardurino/webusb-demo/webusb-demo.ino
+++ ardurino/webusb-demo/webusb-demo.ino
@@ -67,23 +67,25 @@ void loop() {
         Serial.println("Unknown...");
         Serial.flush();
     }
-    void colorWipe(uint32_t color, int wait) {
+  }
+}
+void colorWipe(uint32_t color, int wait) {
   for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
     strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
     strip.show();                          //  Update strip to match
     delay(wait);                           //  Pause for a moment
   }
 }
-    void theaterChase(uint32_t color, int wait) {
-      for(int a=0; a<10; a++) {  // Repeat 10 times...
-        for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-          strip.clear();         //   Set all pixels in RAM to 0 (off)
-          // 'c' counts up from 'b' to end of strip in steps of 3...
-          for(int c=b; c<strip.numPixels(); c += 3) {
-            strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
-          }
-          strip.show(); // Update strip with new contents
-        delay(wait);  // Pause for a moment
+void theaterChase(uint32_t color, int wait) {
+  for(int a=0; a<10; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
     }
   }
 }
